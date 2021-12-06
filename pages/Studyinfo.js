fbb664c8502f0bd2e23887c470904ef09cab97f2
meohyun24 <eogus1954@naver.com>
2021-12-07 01:26:48 +0900

    commit 2021.12.07 01:26

--- pages/Studyinfo.js
+++ pages/Studyinfo.js
@@ -1,35 +1,33 @@
 import React, { useEffect, useState } from 'react'
 import { View,Text ,StyleSheet} from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
-
+import data from '../data'
 
 export default function Studyinfo({navigation,route}){
-    
-    const[list, setlist]= useState({
-        
-        "idx" :0,
-        "name" : "얼죽아는 어떤말의 줄임말?",
-        "answer" : "얼어 죽어도 아메리카노",
-        "options" : ["얼어 죽어도 아메리카노","얼굴이 죽은 아이","얼빠진 죽먹는 아이","얼굴이 죽여주는 아이돌"],
-        "desc_title" : "얼죽아",
-        "desc" : "추운날에도 아메리카노를 아이스로 먹는사람을 일컫는 말"
 
-    })
+    const [list,setlist] = useState({
+            "idx" :0,
+            "name" : "얼죽아는 어떤말의 줄임말?",
+            "answer" : "얼어 죽어도 아메리카노",
+            "options" : ["얼어 죽어도 아메리카노","얼굴이 죽은 아이","얼빠진 죽먹는 아이","얼굴이 죽여주는 아이돌"],
+            "desc_title" : "얼죽아",
+            "desc" : "추운날에도 아메리카노를 아이스로 먹는사람을 일컫는 말"
+
+        })
 
     useEffect(()=>{
-        console.log(route)
-        
+    
         navigation.setOptions({
-            title:"",
+            title:"신조어 설명",
             headerStyle:{
                 backgroundColor : "#000",
                 shadowColor : "#000",
-                borderBottomColor :"#000"
+                borderBottomColor :"#000",
+                height : 100
             }
         })
 
         setlist(route.params)
-
 
     },[])
     
